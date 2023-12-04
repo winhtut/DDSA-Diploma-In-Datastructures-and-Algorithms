651bb055ac92d7b8f0979523a00ad77f324fa2b4
Win Htut <winhtutonline@gmail.com>
2023-12-04 22:19:47 +0630

    1-day

---
+++ 1-Day/1-starting.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main(){
+
+    int age=0;
+
+    printf("Hello coder\nDSA\n");
+    printf("This is from N1C\n");
+
+    printf("Please enter your age:");
+    scanf("%d",&age);
+
+    printf("Your age = %c",age); // x = hexadecimal  c = character
+
+
+
+    return 0;
+
+}
---
+++ 1-Day/2-adding-two-var.c
@@ -0,0 +1,25 @@
+//Adding program by N1C 
+#include<stdio.h> 
+
+int main(){
+    
+    
+    int first_number =0;
+    int sec_number=0;
+    int total=0;
+    
+    printf("This program is for add:\n");
+    printf("Enter your first number:");
+    scanf("%d",&first_number);
+    
+    printf("Enter your second number:");
+    scanf("%d",&sec_number);
+    
+    
+    total = first_number + sec_number; 
+    
+    printf("Your total number: %d =",total);
+    
+    return 0;
+
+}
